18186f9b6c94401696818fbec0b2d172606c5796
Alejandro Piumetti <alepiumetti@hotmail.com>
2020-10-24 17:38:59 -0300

    changes2

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
 var express = require("express");
 var exphbs = require("express-handlebars");
 var app = express();
-var cors = require("cors");
 const mercadopago = require("mercadopago");
 
 // Int
 
 // Agrega credenciales
-mercadopago.configure({
-  access_token:
-    "APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398",
-});
 
 // Settings
-
-app.use(cors());
 
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
@@ -26,6 +19,11 @@ app.get("/", function (req, res) {
 });
 
 app.get("/detail", function (req, res) {
+  mercadopago.configure({
+    access_token:
+      "APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398",
+  });
+
   let preference = {
     items: [
       {
