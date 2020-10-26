8eff78f12927501500290fd02df90c77a8e56adc
Alejandro Piumetti <alepiumetti@hotmail.com>
2020-10-26 18:25:05 -0300

    more changes

--- app.js
+++ app.js
@@ -59,7 +59,9 @@ app.post("/detail", function (req, res) {
         id: "1234",
         title: req.body.title,
         description: "Dispositivo móvil de Tienda e-commerce",
-        picture_url: ("/assets", path.join(__dirname, req.body.img)),
+        picture_url:
+          ("http://alepiumetti-mp-commerce-nodejs.herokuapp.com",
+          path.join(__dirname, req.body.img)),
         unit_price: parseInt(req.body.price),
         quantity: parseInt(req.body.unit),
         currency_id: "ARS",
